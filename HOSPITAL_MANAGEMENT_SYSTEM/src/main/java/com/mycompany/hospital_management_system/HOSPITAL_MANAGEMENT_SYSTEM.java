d49ca9832907ff1bab6a9e2ad9c982fbccabc145
ShashankDongre <95865448+ShashankDongre@users.noreply.github.com>
2022-10-22 23:41:43 -0400

    Home Page Initial commit
    Initial commit of the proposed home page of the application.

---
+++ HOSPITAL_MANAGEMENT_SYSTEM/src/main/java/com/mycompany/hospital_management_system/HOSPITAL_MANAGEMENT_SYSTEM.java
@@ -0,0 +1,23 @@
+
+
+package com.mycompany.hospital_management_system;
+
+/**
+ *
+ * @author Shashank
+ */
+
+public class HOSPITAL_MANAGEMENT_SYSTEM {
+    
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+        
+        
+        
+    }
+    
+    
+    
+}
